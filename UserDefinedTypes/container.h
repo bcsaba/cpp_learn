79b5882deea2483281b1b33bbd5d990b52925502
Csaba Benko <csabi@xps25>
2025-10-24 16:03:33 +0200

    Added container declaration. No implementation yet.

---
+++ UserDefinedTypes/container.h
@@ -0,0 +1,18 @@
+//
+// Created by csabi on 24/10/2025.
+//
+
+#ifndef USERDEFINEDTYPES_CONTAINER_H
+#define USERDEFINEDTYPES_CONTAINER_H
+
+namespace mycode {
+    class Container {
+    public:
+        virtual double operator[](int) = 0;
+        [[nodiscard]] virtual int size() const = 0;
+        // virtual ~Container() {}
+        virtual ~Container() = default;
+    };
+}
+
+#endif //USERDEFINEDTYPES_CONTAINER_H
